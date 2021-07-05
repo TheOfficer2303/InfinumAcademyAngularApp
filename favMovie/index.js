d2e0035a46c9257fd55654fc8db3834f7a88e821
blaks <blazsolic99@gmail.com>
2021-07-05 22:55:27 +0200

    Add rendering comments

--- favMovie/index.js
+++ favMovie/index.js
@@ -3,6 +3,15 @@ let comments = [];
 let commentsListElement = document.getElementById("comments-list");
 function renderComment(comment) {
     const listItem = document.createElement("li");
+    
+    const text = document.createElement("p");
+    const rating = document.createElement("p");
+
+    text.textContent = comment.text;
+    rating.textContent = comment.rating;
+
+    listItem.appendChild(text)
+    listItem.appendChild(rating)
 
     commentsListElement.appendChild(listItem);
 }
@@ -12,14 +21,16 @@ const commentFormElement = document.getElementById("comment-form");
 commentFormElement.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const commentText = document.getElementById("comment-text");
-    const commentRating = document.getElementById("rating-number");
+    const formData = new FormData(commentFormElement);
+
+    const commentText = formData.get("comment-text");
+    const commentRating = formData.get("rating-number");
 
     let comment = {
         text: commentText,
         rating: commentRating
     }
-    console.log("Submitano")
+    console.log(comment)
 
     comments.push(comment);
     renderComment(comment);
