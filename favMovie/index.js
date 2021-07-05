b92076279352dd5a993ff7d76fea08b3c3e6c2cc
blaks <blazsolic99@gmail.com>
2021-07-06 00:10:42 +0200

    Add remove button for comments

--- favMovie/index.js
+++ favMovie/index.js
@@ -1,41 +1,104 @@
 let comments = [];
+let ids = [];
 
 let commentsListElement = document.getElementById("comments-list");
+
 function renderComment(comment) {
-    const listItem = document.createElement("li");
-    
-    const text = document.createElement("p");
-    const rating = document.createElement("p");
+	const listItem = document.createElement("li");
 
-    text.textContent = comment.text;
-    rating.textContent = comment.rating;
+	const text = document.createElement("p");
+	const rating = document.createElement("p");
 
-    listItem.appendChild(text)
-    listItem.appendChild(rating)
+	const removeButton = document.createElement("button");
+	removeButton.type = "button";
+	removeButton.textContent = "Remove";
+	removeButton.classList.add("remove-comment");
+	removeButton.setAttribute("id", comment.identifier);
+	console.log(removeButton);
 
-    commentsListElement.appendChild(listItem);
+	text.textContent = comment.text;
+	rating.textContent = comment.rating;
+
+	listItem.appendChild(text)
+	listItem.appendChild(rating)
+	listItem.appendChild(removeButton);
+
+	commentsListElement.appendChild(listItem);
+	removingComments();
 }
+
+
+
+
 
 const commentFormElement = document.getElementById("comment-form");
 
 commentFormElement.addEventListener("submit", function (event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    const formData = new FormData(commentFormElement);
+	const formData = new FormData(commentFormElement);
 
-    const commentText = formData.get("comment-text");
-    const commentRating = formData.get("rating-number");
+	const commentText = formData.get("comment-text");
+	const commentRating = formData.get("rating-number");
 
-    let comment = {
-        text: commentText,
-        rating: commentRating
-    }
-    console.log(comment)
+	let comment = {
+		text: commentText,
+		rating: commentRating,
+		identifier: getAvailableID()
+	}
+	console.log(comment)
 
-    comments.push(comment);
-    renderComment(comment);
+	comments.push(comment);
+	renderComment(comment);
 });
 
-for (const comment of comments) {
-    renderComment(comment);
+function getAvailableID() {
+	ids.push(1);
+
+	return ids.length;
 }
+
+function renderComments() {
+	for (const comment of comments) {
+		renderComment(comment);
+	}
+}
+
+function removingComments() {
+	document.querySelectorAll(".remove-comment")
+		.forEach(button => {
+			button.addEventListener("click", event => {
+				if (event.target.className === "remove-comment") {
+					removeComment(button.id);
+				}
+			})
+		});
+}
+
+function removeComment(commentId) {
+	let commentToRemove = undefined;
+
+	for (const comment of comments) {
+        console.log(comment.identifier, commentId)
+		if (comment.identifier == commentId) {
+           
+			commentToRemove = comment;
+		}
+	}
+    
+
+    commentsListElement.removeChild(document.getElementById(commentId).parentElement);
+
+	const index = comments.indexOf(commentToRemove);
+	if (index > -1) {
+		comments.splice(index, 1);
+	}
+    console.log(comments);
+
+    
+    
+}
+
+
+renderComments();
+removingComments();
