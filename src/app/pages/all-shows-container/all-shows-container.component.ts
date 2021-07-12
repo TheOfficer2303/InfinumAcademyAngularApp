5188df9b026d7a5973305d93e99d935fd1fda715
blaks <blazsolic99@gmail.com>
2021-07-12 13:16:11 +0200

    Minor changes

--- src/app/pages/all-shows-container/all-shows-container.component.ts
+++ src/app/pages/all-shows-container/all-shows-container.component.ts
@@ -18,25 +18,25 @@ export class AllShowsContainerComponent implements OnInit {
 	mockData: Array < any > = [{
 			title: 'The Office',
 			description: 'Some text',
-			average_rating: 8,
+			average_rating: 5,
 			image_url: 'https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg'
 		},
 		{
-			title: 'Јужни Ветар',
+			title: 'Južni Vetar',
 			description: 'Some text',
-			average_rating: 7,
+			average_rating: 3,
 			image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8SzwV858xAjuiODRgfP2SKTjHcjQIb6os9tIC2Vu4OyrJQmqaZ3Y11pesuGFuSYB65Qc&usqp=CAU'
 		},
 		{
 			title: 'La Casa de Papel',
 			description: 'Some text',
-			average_rating: 9,
+			average_rating: 5,
 			image_url: 'https://www.avoir-alire.com/IMG/arton42400.png'
 		},
 		{
 			title: 'South Park',
 			description: 'Some text',
-			average_rating: 10,
+			average_rating: 4,
 			image_url: 'https://southparkstudios.mtvnimages.com/uri/mgid:arc:content:shared.southpark.us.en:17ba224c-7a62-4a92-be6c-3144b6d80a48?quality=0.7'
 		}
 	]
@@ -45,7 +45,9 @@ export class AllShowsContainerComponent implements OnInit {
   
 	ngOnInit(): void {
     this.shows = this.mockData.map((show) => {
-      return new Show(show)
+			show = new Show(show);
+			console.log(show.ratingInPercentage())
+      return show;
     })
 		console.log(this.shows)
   }
