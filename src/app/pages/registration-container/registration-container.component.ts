e8c9cfdd88094c58df75335a7adba1148ff427ea
blaks <blazsolic99@gmail.com>
2021-07-20 19:12:15 +0200

    Add progress bar while waiting for server response

--- src/app/pages/registration-container/registration-container.component.ts
+++ src/app/pages/registration-container/registration-container.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { UserFormData } from './components/registration-form/registration-form.component';
 
@@ -11,11 +12,16 @@ import { UserFormData } from './components/registration-form/registration-form.c
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegistrationContainerComponent {
+  public isLoading$:Subject<boolean> = new Subject<boolean>()
+
   constructor(private authService: AuthService, private router: Router) { }
 
   public onRegisterUser(userFormData: UserFormData) {
+    this.isLoading$.next(true)
     this.authService.registerUser(userFormData).pipe(
-      tap(console.log)
+      finalize(() => {
+        this.isLoading$.next(false);
+      })
     )
     .subscribe((userData: UserFormData) => {
       this.router.navigate(['']);
