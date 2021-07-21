abb060dc6627d9fa3b0e714a5efdacd47d113381
blaks <blazsolic99@gmail.com>
2021-07-21 16:54:50 +0200

    Add method to login user

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { LoginData } from 'src/app/pages/login-container/login-form/login-form.component';
 import { UserFormData } from 'src/app/pages/registration-container/components/registration-form/registration-form.component';
 
 @Injectable({
@@ -11,7 +13,11 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   public registerUser(userFormData: UserFormData): Observable<UserFormData> {
-    console.log(userFormData)
     return this.http.post<UserFormData>('https://tv-shows.infinum.academy/users', userFormData);
+  }
+
+  public loginUser(loginData: LoginData): Observable<LoginData>{
+    return this.http.post<LoginData>('https://tv-shows.infinum.academy/users/sign_in', loginData).pipe(
+    )
   }
 }
