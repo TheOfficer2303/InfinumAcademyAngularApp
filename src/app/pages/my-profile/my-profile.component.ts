141cff94e49382b6c75bca09aea08c07313eef69
blaks <blazsolic99@gmail.com>
2021-08-03 21:04:10 +0200

    Implement basic user image upload

--- src/app/pages/my-profile/my-profile.component.ts
+++ src/app/pages/my-profile/my-profile.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { UploadService } from 'src/app/services/upload/upload.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,8 +10,24 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./my-profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MyProfileComponent {
-  public email = this.authService.getAuthData()?.uid
 
-  constructor(private authService: AuthService) { }
+export class MyProfileComponent {
+  public email = this.authService.getAuthData()?.uid;
+
+  constructor(private authService: AuthService, 
+              private uploadService: UploadService, 
+              private snackBar: MatSnackBar) { }
+
+  public upload(image: any) {
+    this.uploadService.upload(image).subscribe({
+      error: (errResponse: HttpErrorResponse) => {
+        console.log(errResponse)
+        this.snackBar.open(errResponse.error.errors.full_messages[0], 'Close', {
+          duration: 3500
+        })
+      }
+    }
+      
+    );
+  }
 }
